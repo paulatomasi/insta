1834436251e2a17cb689028cc82ce1ea83c8246f
Paula Tomasi <paula.andrade@cwi.com.br>
2020-01-31 15:29:36 -0300

    adding navigation

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-};
+  env: {
+    production: {
+      plugins: ['transform-remove-console'],
+    },
+  },
+  plugins: [
+    ['module-resolver',
+      {
+        alias: {
+          screens: './src/screens/',
+        },
+      },
+    ],
+  ],
+}
